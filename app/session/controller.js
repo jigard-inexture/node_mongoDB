9740e8fbc6a8fa456933a1463c7b39ad915bff60
jigard-inexture <jigard.inexture@gmail.com>
2019-01-02 10:30:30 +0530

    first commit

---
+++ app/session/controller.js
@@ -0,0 +1,30 @@
+const jwt = require( "jsonwebtoken" );
+const repository = require( "./repository" );
+
+const config = require( "../../config" );
+const logger = require( "../../utilities/logger" );
+
+exports.login = async ( req, res ) => {
+    console.log('req.body',req.body);
+    if ( !req.body.password || !req.body.username ) {
+        res.preconditionFailed( "Credentials required" );
+        return;
+    }
+
+    try {
+        const user = await repository.findUser( req.body );
+        if ( !user || !user.checkPass( req.body.password ) ) {
+            res.json( { success: false, message: "Authentication failed." } );
+            return;
+        }
+
+        const token = jwt.sign( user.toObject(), config.secret, { expiresIn: 3600 } );
+        logger.info( "User loged in with success. Login token", token );
+        res.json( {
+            success: true,
+            token,
+        } );
+    } catch ( err ) {
+        res.send( err );
+    }
+};
---
+++ config/environments/development.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: "localhost",
+    port: 1234, // change with development port
+    mongoUrl: "mongodb://localhost:27017/inexture", // replace "projectDbName" with a proper db name
+    logLevel: "debug", // can be chenged to error, warning, info, verbose or silly
+    secret: "superSuperSecret",
+};
